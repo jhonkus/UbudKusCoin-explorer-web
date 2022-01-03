e065d9bd7be16eb99bb50228c12a9bc04c9bdb8f
jhonkus <putukn@gmail.com>
2022-01-03 08:02:05 +0800

    TESING

--- pages/api/blocks.ts
+++ pages/api/blocks.ts
@@ -23,17 +23,14 @@ export default function handler(req, res) {
         grpc.credentials.createInsecure()
     );
     
-    client.GetBlocks({page_number: 1, result_per_page: 20} , function(err, response) {
-        // console.log('Data:', response); // API response
-        // console.log(err);
+    // client.GetBlocks({page_number: 1, result_per_page: 20} , function(err, response) {
+    //     // console.log('Data:', response); // API response
+    //     // console.log(err);
 
-        if (!err) {
-            res.status(200).json(response)
-        } else {
-            res.status(200).json(err)
-        }
-       });
+    //     res.status(200).json(response)
+    
+    // });
     
     
-    // res.status(200).json('finish')
+    res.status(200).json('finish')
 }
