0ed20c153cd2b7e719ae58cb8bd876d4c541cd6a
jhonkus <putukn@gmail.com>
2022-01-03 09:08:14 +0800

    testing 20

--- pages/api/blocks.ts
+++ pages/api/blocks.ts
@@ -1,62 +1,44 @@
 import path from 'path'
 // import getConfig from 'next/config'
 
-// const grpc = require('@grpc/grpc-js');
-// const protoLoader = require("@grpc/proto-loader");
-// const PROTO_PATH = __dirname + "/bchain.proto";
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require("@grpc/proto-loader");
 
-// const host = "54.254.238.133:5002";
+const dirRelativeToPublicFolder = 'protos'
+const dir = path.resolve('./public', dirRelativeToPublicFolder);
+const PROTO_PATH = dir + '/bchain.proto';
+const host = "54.254.238.133:5002";
 
-// const options = {
-//     keepCase: true,
-//     longs: String,
-//     enums: String, 
-//     defaults: true,
-//     oneofs: true,
-// };
+const options = {
+    keepCase: true,
+    longs: String,
+    enums: String, 
+    defaults: true,
+    oneofs: true,
+};
 
-// const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
 
-// const proto = grpc.loadPackageDefinition(packageDefinition);
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+
+const proto = grpc.loadPackageDefinition(packageDefinition);
 
 
 export default function handler(req, res) {
-    // const client = new proto.BChainService(
-    //     host,
-    //     grpc.credentials.createInsecure()
-    // );
-
-    // const { serverRuntimeConfig } = getConfig()
-
-    // client.GetBlocks({ page_number: 1, result_per_page: 20 }, function (err, response) {
- 
-    //     if (!err) {
-    //         res.status(200).json(response)
-    //     } else {
-    //         res.status(200).json(err)
-    //     }
-    // });
-
-
-    const dirRelativeToPublicFolder = 'img'
-
-    const dir = path.resolve('./public', dirRelativeToPublicFolder);
-  
-    // const filenames = fs.readdirSync(dir);
-  
-    // const proto = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
-  
-    // const packageDefinition = protoLoader.loadSync(proto, options)
-
-    // const dirRelativeToPublicFolder = 'img'
-
-//    const dir = path.join(serverRuntimeConfig.PROJECT_ROOT, './public', dirRelativeToPublicFolder);
-  
-    // const filenames = fs.readdirSync(dir);
+    const client = new proto.BChainService(
+        host,
+        grpc.credentials.createInsecure()
+    );
 
     
-    res.statusCode = 200
-    res.json({file: dir});
+    client.GetBlocks({ page_number: 1, result_per_page: 20 }, function(err, response) {
+ 
+        if (!err) {
+            res.status(200).json(response)
+        } else {
+            res.status(200).json(err)
+        }
+    });
+
 
     // res.status(200).json('finish')
 }
