bfbba505359ad53ed1bce32d107401a6ffa15c54
jhonkus <putukn@gmail.com>
2022-01-10 06:26:15 +0800

    add loading gif

--- components/status/LoadingComp.tsx
+++ components/status/LoadingComp.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import loading from "../../public/loading.gif";
+
 const LoadingComp = () => {
     return (
-        <div className="d-flex justify-content-center">
-            <br/>
-            <p>Please wait loading ...</p>
-        </div>
+        <main id="main" className="main">
+            <div className="d-flex justify-content-center">
+                <Image src={loading} width="20" height="20" alt="Please wait loading ..." />
+            </div>
+        </main>
     )
 }
 export default LoadingComp;
