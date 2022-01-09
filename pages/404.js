569a397247738dbb118865d3c7bf1c12dd2afca4
jhonkus <putukn@gmail.com>
2022-01-10 06:54:04 +0800

    custom 404

--- pages/404.js
+++ pages/404.js
@@ -2,8 +2,9 @@ export default function Custom404() {
     return (
         <main id="main" className="main">
             <div className="d-flex justify-content-center">
-                <p>Sorry, page you are looking not found.<br />
-                    Please visit visit other page!</p>
+                <p>Thank you visiting our website, but<br />
+                    Sorry, page you are looking not found.<br />
+                    Please visit other page!</p>
             </div>
         </main>
     )
