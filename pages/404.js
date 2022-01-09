3373d2b09794a94cd8b28d18cad61530833fb726
jhonkus <putukn@gmail.com>
2022-01-10 06:50:08 +0800

    add custome not found page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+export default function Custom404() {
+    return (
+        <main id="main" className="main">
+            <div className="d-flex justify-content-center">
+                <p>Sorry, page you are looking not found.<br />
+                    Please visit visit other page!</p>
+            </div>
+        </main>
+    )
+}
--- pages/block/index.tsx
+++ pages/block/index.tsx
@@ -1,13 +1,13 @@
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
 
-export default function Address(){
+export default function NoTxnId() {
     return (
         <>
             <Header />
             <main id="main" className="main">
                 <div className="d-flex justify-content-center">
-                    <p>Please visit this page with Account address!</p>
+                    <p>Please visit this page with block Height or Block Hash!</p>
                 </div>
             </main>
             <Footer />
