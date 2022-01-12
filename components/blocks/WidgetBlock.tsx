e0881db56afb5e2fcfae514154bd383418edd7ad
jhonkus <putukn@gmail.com>
2022-01-13 07:26:18 +0800

    update link in block wallet

--- components/blocks/WidgetBlock.tsx
+++ components/blocks/WidgetBlock.tsx
@@ -11,7 +11,7 @@ import styles from './Blocks.module.css';
  */
 const WidgetBlocks = () => {
   const { blocks, isLoading, isError } = getBlocks(1, 10);
-  
+
   if (isLoading) return <LoadingComp />
   if (isError) return <ErrorComp />
 
@@ -26,7 +26,7 @@ const WidgetBlocks = () => {
           <div className={`row ${styles.divRow}`} key={block.Height}>
             <div className="col-sm-1 align-self-center">
               <div className={styles.bk}>
-              <i className="bi bi-box"></i>
+                <i className="bi bi-box"></i>
               </div>
             </div>
             <div className="col-sm-3 d-flex flex-column">
@@ -36,14 +36,19 @@ const WidgetBlocks = () => {
               <span className={styles.dateTx}>{timeAgo(block.TimeStamp)}</span>
             </div>
             <div className="col-sm-5 d-flex flex-column">
-              <span className={styles.validatorLabel}>Validator : <span className={styles.validator}>
+              <span className={styles.validatorLabel}>Block maker : <span className={styles.validator}>
                 <Link href={`/address/${block.Validator}`}>
                   <a>
                     {block.Validator?.substring(0, 16)}...
                   </a>
                 </Link>
               </span></span>
-              <span className={styles.numTx}>{block.NumOfTx} <span className={styles.lblTx}>txns</span></span>
+              <span className={styles.numTx}>
+                <Link href={`/txns/block/${block.Height}`}>
+                  <a className={styles.valueWithLink}>
+                  {block.NumOfTx} txns </a>
+                </Link>
+                <span className={styles.lblTx}> in this block</span></span>
             </div>
             <div className="col-sm-3 align-self-center">
               <div className={`align-self-end ${styles.amount}`}>{formatAmount(block.TotalAmount)} Ukc</div>
