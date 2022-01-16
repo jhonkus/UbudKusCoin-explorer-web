5a885808d5c0260f907ba70be26287b1cdfb5d54
jhonkus <putukn@gmail.com>
2022-01-16 10:39:14 +0800

    update 404

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,15 @@
+import Layout from '../components/Layout'
+
 export default function Custom404() {
     return (
-        <main id="main" className="main">
-            <div className="d-flex justify-content-center">
-                <p>Thank you visiting our website, but<br />
-                    Sorry, page you are looking not found.<br />
-                    Please visit other page!</p>
-            </div>
-        </main>
+        <Layout pageTitle="Please visit by block height">
+            <main id="main" className="main">
+                <div className="d-flex justify-content-center">
+                    <p>Thank you visiting UKC Scan,<br />
+                        but sorry, page you are looking not found.<br />
+                        Please visit other page!</p>
+                </div>
+            </main>
+        </Layout>
     )
 }
