dbcf6833637bbbb91399bba8c08903612b094146
jhonkus <putukn@gmail.com>
2022-01-20 22:03:05 +0800

    update pending txn

--- components/transactions/TablePendingTxns.tsx
+++ components/transactions/TablePendingTxns.tsx
@@ -3,8 +3,9 @@ import { getPendingTxns } from '../../grpc/useFetch';
 import { timeAgo, formatAmount, formatFee } from '../../utils/util';
 import styles from './Txns.module.css'
 
-import Image from "next/image";
-import loading from "../../public/loading.gif";
+// import Image from "next/image";
+// import loading from "../../public/loading.gif";
+import Skeleton from 'react-loading-skeleton';
 
 
 
@@ -14,8 +15,8 @@ import loading from "../../public/loading.gif";
  */
 const TablePendingTxns = ({ page = 1 }) => {
   const { transactions, isLoading, isError } = getPendingTxns();
-  if (isLoading) return <div><Image src={loading} width="20" height="20" alt="Please wait loading ..." /></div>
-  if (isError) return <div><p>Error when loading</p></div>
+  // if (isLoading) return <div><Image src={loading} width="20" height="20" alt="Please wait loading ..." /></div>
+  // if (isError) return <div><p>Error when loading</p></div>
 
   return (
 
@@ -23,75 +24,86 @@ const TablePendingTxns = ({ page = 1 }) => {
 
       <div className="card-body">
         <div className="card-title" />
-        <div className="row">
-        <div className="col d-flex justify-content-start">
-            <p>Number of pending transactions : <strong>{transactions?.length}</strong></p>
-          </div>
-          <div className="col d-flex justify-content-end">
-            <p>Auto update every <strong>5</strong> seconds</p>
-          </div>
-        </div>
 
-        <div className="table-responsive">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th className={styles.tableHeader}>Txn Hash</th>
-                <th className={styles.tableHeader}>Age</th>
-                <th className={styles.tableHeader}>From</th>
-                <th className={styles.tableHeader}>To</th>
-                <th className={styles.tableHeader}>Value</th>
-                <th className={styles.tableHeader}>Fee</th>
-              </tr>
-            </thead>
-            <tbody>
+        {(!transactions && !isLoading && !isError) &&
+          <div className="text-center"><p>Blocks not found! </p></div>
+        }
+        {(isLoading || isError) && <Skeleton count={10} />}
+        {transactions &&
 
-              {transactions?.map((tx) => (
+          <>
 
-                <tr key={tx.Hash}>
-                  <td>
-                    <span className={styles.hashTx}>{tx.Hash.substring(0, 15)}...
-                    </span>
-                  </td>
-                  <td>
-                    <span className={styles.dateTx}>{timeAgo(tx.TimeStamp)}</span>
-                  </td>
-                  <td className={styles.address}>
-                    <Link href={`/address/${tx.Sender}`}>
-                      <a>
-                        <span className={styles.addrsInTable}>
-                          {tx.Sender.substring(0, 20)}...
+            <div className="row">
+              <div className="col d-flex justify-content-start">
+                <p>Number of pending transactions : <strong>{transactions?.length}</strong></p>
+              </div>
+              <div className="col d-flex justify-content-end">
+                <p>Auto update every <strong>5</strong> seconds</p>
+              </div>
+            </div>
+
+            <div className="table-responsive">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th className={styles.tableHeader}>Txn Hash</th>
+                    <th className={styles.tableHeader}>Age</th>
+                    <th className={styles.tableHeader}>From</th>
+                    <th className={styles.tableHeader}>To</th>
+                    <th className={styles.tableHeader}>Value</th>
+                    <th className={styles.tableHeader}>Fee</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  {transactions?.map((tx) => (
+
+                    <tr key={tx.Hash}>
+                      <td>
+                        <span className={styles.hashTx}>{tx.Hash.substring(0, 15)}...
                         </span>
-                      </a>
-                    </Link>
-                  </td>
-                  <td>
-                    <Link href={`/address/${tx.Recipient}`}>
-                      <a>
-                        <span className={styles.addrsInTable}>
-                          {tx.Recipient.substring(0, 20)}...
-                        </span>
-                      </a>
-                    </Link>
-                  </td>
-                  <td>
-                    <div className={styles.amountInTable}>{formatAmount(tx.Amount)}</div>
-                  </td>
-                  <td>
-                    <div className={styles.amountInTable}>{formatFee(tx.Fee)}</div>
-                  </td>
-                </tr>
+                      </td>
+                      <td>
+                        <span className={styles.dateTx}>{timeAgo(tx.TimeStamp)}</span>
+                      </td>
+                      <td className={styles.address}>
+                        <Link href={`/address/${tx.Sender}`}>
+                          <a>
+                            <span className={styles.addrsInTable}>
+                              {tx.Sender.substring(0, 20)}...
+                            </span>
+                          </a>
+                        </Link>
+                      </td>
+                      <td>
+                        <Link href={`/address/${tx.Recipient}`}>
+                          <a>
+                            <span className={styles.addrsInTable}>
+                              {tx.Recipient.substring(0, 20)}...
+                            </span>
+                          </a>
+                        </Link>
+                      </td>
+                      <td>
+                        <div className={styles.amountInTable}>{formatAmount(tx.Amount)}</div>
+                      </td>
+                      <td>
+                        <div className={styles.amountInTable}>{formatFee(tx.Fee)}</div>
+                      </td>
+                    </tr>
 
-              ))}
+                  ))}
 
-            </tbody>
-          </table>
-        </div>
-        <div className="row">
-          <div className="col d-flex justify-content-end">
-      
-          </div>
-        </div>
+                </tbody>
+              </table>
+            </div>
+            <div className="row">
+              <div className="col d-flex justify-content-end">
+
+              </div>
+            </div>
+          </>
+        }
       </div>
     </div>
   )
--- components/transactions/TableTxns.tsx
+++ components/transactions/TableTxns.tsx
@@ -2,9 +2,7 @@ import Link from 'next/link'
 import { getTxns } from '../../grpc/useFetch';
 import { timeAgo, formatAmount, formatFee } from '../../utils/util';
 import styles from './Txns.module.css'
-
-import Image from "next/image";
-import loading from "../../public/loading.gif";
+import Skeleton from 'react-loading-skeleton';
 
 const Pagination = ({ pageNum = 1 }) => {
   return (
@@ -30,8 +28,8 @@ const Pagination = ({ pageNum = 1 }) => {
  */
 const TableTxns = ({ page = 1 }) => {
   const { transactions, isLoading, isError } = getTxns(page, 25);
-  if (isLoading) return <div><Image src={loading} width="20" height="20" alt="Please wait loading ..." /></div>
-  if (isError) return <div><p>Error when loading</p></div>
+  // if (isLoading) return <div><Image src={loading} width="20" height="20" alt="Please wait loading ..." /></div>
+  // if (isError) return <div><p>Error when loading</p></div>
 
   return (
 
@@ -39,82 +37,94 @@ const TableTxns = ({ page = 1 }) => {
 
       <div className="card-body">
         <div className="card-title" />
-        <div className="row">
-          <div className="col d-flex justify-content-start">
-            <p>Showing 25 transactions</p>
-          </div>
-          <div className="col d-flex justify-content-end">
-            <Pagination pageNum={page} />
-          </div>
-        </div>
 
-        <div className="table-responsive">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th className={styles.tableHeader}>Txn Hash</th>
-                <th className={styles.tableHeader}>Block</th>
-                <th className={styles.tableHeader}>Age</th>
-                <th className={styles.tableHeader}>From</th>
-                <th className={styles.tableHeader}>To</th>
-                <th className={styles.tableHeader}>Value</th>
-                <th className={styles.tableHeader}>Fee</th>
-              </tr>
-            </thead>
-            <tbody>
 
-              {transactions?.map((tx) => (
+        {(!transactions && !isLoading && !isError) &&
+          <div className="text-center"><p>Blocks not found! </p></div>
+        }
+        {(isLoading || isError) && <Skeleton count={10} />}
+        {transactions &&
 
-                <tr key={tx.Hash}>
-                  <td>
-                    <Link href={`/txns/${tx.Hash}`}><a><span className={styles.hashTx}>{tx.Hash.substring(0, 15)}...
-                    </span></a></Link>
-                  </td>
-                  <td>
+          <>
+            <div className="row">
+              <div className="col d-flex justify-content-start">
+                <p>Showing 25 transactions</p>
+              </div>
+              <div className="col d-flex justify-content-end">
+                <Pagination pageNum={page} />
+              </div>
+            </div>
 
-                    <Link href={`/blocks/height/${tx.Height}`}><a>
-                      <span className={styles.hashTx}>{tx.Height}
-                      </span></a></Link>
-                  </td>
-                  <td>
-                    <span className={styles.dateTx}>{timeAgo(tx.TimeStamp)}</span>
-                  </td>
-                  <td className={styles.address}>
-                    <Link href={`/address/${tx.Sender}`}>
-                      <a>
-                        <span className={styles.addrsInTable}>
-                          {tx.Sender.substring(0, 20)}...
-                        </span>
-                      </a>
-                    </Link>
-                  </td>
-                  <td>
-                    <Link href={`/address/${tx.Recipient}`}>
-                      <a>
-                        <span className={styles.addrsInTable}>
-                          {tx.Recipient.substring(0, 20)}...
-                        </span>
-                      </a>
-                    </Link>
-                  </td>
-                  <td>
-                    <div className={styles.amountInTable}>{formatAmount(tx.Amount)}</div>
-                  </td>
-                  <td>
-                    <div className={styles.amountInTable}>{formatFee(tx.Fee)}</div>
-                  </td>
-                </tr>
+            <div className="table-responsive">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th className={styles.tableHeader}>Txn Hash</th>
+                    <th className={styles.tableHeader}>Block</th>
+                    <th className={styles.tableHeader}>Age</th>
+                    <th className={styles.tableHeader}>From</th>
+                    <th className={styles.tableHeader}>To</th>
+                    <th className={styles.tableHeader}>Value</th>
+                    <th className={styles.tableHeader}>Fee</th>
+                  </tr>
+                </thead>
+                <tbody>
 
-              ))}
+                  {transactions?.map((tx) => (
 
-            </tbody>
-          </table>
-        </div>
-        <div className="row">
-          <div className="col d-flex justify-content-end">
-            <Pagination pageNum={page} />
-          </div>
-        </div>
+                    <tr key={tx.Hash}>
+                      <td>
+                        <Link href={`/txns/${tx.Hash}`}><a><span className={styles.hashTx}>{tx.Hash.substring(0, 15)}...
+                        </span></a></Link>
+                      </td>
+                      <td>
+
+                        <Link href={`/blocks/height/${tx.Height}`}><a>
+                          <span className={styles.hashTx}>{tx.Height}
+                          </span></a></Link>
+                      </td>
+                      <td>
+                        <span className={styles.dateTx}>{timeAgo(tx.TimeStamp)}</span>
+                      </td>
+                      <td className={styles.address}>
+                        <Link href={`/address/${tx.Sender}`}>
+                          <a>
+                            <span className={styles.addrsInTable}>
+                              {tx.Sender.substring(0, 20)}...
+                            </span>
+                          </a>
+                        </Link>
+                      </td>
+                      <td>
+                        <Link href={`/address/${tx.Recipient}`}>
+                          <a>
+                            <span className={styles.addrsInTable}>
+                              {tx.Recipient.substring(0, 20)}...
+                            </span>
+                          </a>
+                        </Link>
+                      </td>
+                      <td>
+                        <div className={styles.amountInTable}>{formatAmount(tx.Amount)}</div>
+                      </td>
+                      <td>
+                        <div className={styles.amountInTable}>{formatFee(tx.Fee)}</div>
+                      </td>
+                    </tr>
+
+                  ))}
+
+                </tbody>
+              </table>
+            </div>
+            <div className="row">
+              <div className="col d-flex justify-content-end">
+                <Pagination pageNum={page} />
+              </div>
+
+            </div>
+          </>
+        }
       </div>
     </div>
   )
