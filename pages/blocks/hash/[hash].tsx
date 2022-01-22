6cc6b12d76a50524a22636696e80a3791e55cdc8
jhonkus <putukn@gmail.com>
2022-01-22 21:48:57 +0800

    add tooltips on block hash

--- pages/blocks/hash/[hash].tsx
+++ pages/blocks/hash/[hash].tsx
@@ -2,12 +2,14 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 import { GetBlockByHash } from '../../../grpc/useFetch';
-import { formatAmount, formatBytes, timeAgo, toDate } from '../../../utils/util';
+import { formatAmount, formatBytes, formatFee, timeAgo, toDate } from '../../../utils/util';
 import styles from '../Block.module.css';
 
 import { useEffect, useState } from 'react';
 import Layout from '../../../components/Layout';
 import Skeleton from 'react-loading-skeleton';
+
+import HelpTips from '../../../components/helptips/help';
 
 export default function BlockHash() {
   const router = useRouter()
@@ -65,79 +67,97 @@ export default function BlockHash() {
                   }
                   {(isLoading || isError) && <Skeleton count={10} />}
                   {block &&
-                    <>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Block Height </div>
-                        <div className={`col-sm-8 ${styles.value}`}><strong>{block.Height}</strong>&nbsp;&nbsp;
-                          <Link href={`/blocks/height/${prevHeight}`}>
-                            <a><i className="bi bi-chevron-left"></i></a>
-                          </Link>
-                          &nbsp;&nbsp;
-                          <Link href={`/blocks/height/${nextHeight}`}>
-                            <a><i className="bi bi-chevron-right"></i></a>
-                          </Link>
-                        </div>
-                      </div>
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Timestamp </div>
-                        <div className={`col-sm-8 ${styles.value}`}><i className="bi bi-clock"></i> {timeAgo(block.TimeStamp)} ({toDate(block.TimeStamp)})</div>
-                      </div>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Transactions </div>
-                        <div className={`col-sm-8 ${styles.value}`}>
-                          <Link href={`/txns/block/${block.Height}`}>
-                            <a className={styles.valueWithLink}>{block.NumOfTx} transaction(s) </a>
-                          </Link>
-                          in this block</div>
-                      </div>
-
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Validator</div>
-                        <div className={`col-sm-8 ${styles.value}`}>
-                          <Link href={`/address/${block.Validator}`}>
-                            <a className={styles.valueWithLink}>{block.Validator}</a>
-                          </Link>
-                          &nbsp;&nbsp; in {block.BuildTime} ms
-                        </div>
-                      </div>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Total Amount</div>
-                        <div className={`col-sm-8 ${styles.value}`}><strong>{formatAmount(block.TotalAmount)}</strong> uks</div>
-                      </div>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Block Reward</div>
-                        <div className={`col-sm-8 ${styles.value}`}><strong>{formatAmount(block.TotalReward)}</strong> uks</div>
-                      </div>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Merkle Root </div>
-                        <div className={`col-sm-8 ${styles.value}`}>{block.MerkleRoot}</div>
-                      </div>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Size</div>
-                        <div className={`col-sm-8 ${styles.value}`}>{formatBytes(block.Size)}  bytes</div>
-                      </div>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Hash </div>
-                        <div className={`col-sm-8 ${styles.value}`}>{block.Hash}</div>
-                      </div>
-
-                      <div className={`row ${styles.rowDiv}`}>
-                        <div className="col-sm-4">Prev. hash </div>
-                        <div className={`col-sm-8`}>
-                          <Link href={`/blocks/hash/${block.PrevHash}`}>
-                            <a className={styles.valueWithLink}>{block.PrevHash}</a>
-                          </Link>
-                        </div>
-                      </div>
-                    </>
+                             <>
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4"><HelpTips tips={'Block height also known as block number is length of blockchain.'} />Block Height </div>
+                               <div className={`col-sm-8 ${styles.value}`}><strong>{block.Height}</strong>&nbsp;&nbsp;
+                                 <Link href={`/blocks/height/${prevHeight}`}>
+                                   <a><i className="bi bi-chevron-left"></i></a>
+                                 </Link>
+                                 &nbsp;&nbsp;
+                                 <Link href={`/blocks/height/${nextHeight}`}>
+                                   <a><i className="bi bi-chevron-right"></i></a>
+                                 </Link>
+                               </div>
+                             </div>
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'The date and time at wwhich a block created.'} />
+                                 Timestamp </div>
+                               <div className={`col-sm-8 ${styles.value}`}><i className="bi bi-clock"></i> {timeAgo(block.TimeStamp)} ({toDate(block.TimeStamp)})</div>
+                             </div>
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'The number of transactions in the block.'} />
+                                 Transactions </div>
+                               <div className={`col-sm-8 ${styles.value}`}>
+                                 <Link href={`/txns/block/${block.Height}`}>
+                                   <a className={styles.valueWithLink}>{block.NumOfTx} transaction(s) </a>
+                                 </Link>
+                                 in this block</div>
+                             </div>
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'Total amount of all transactions in this block.'} />
+                                 Amount</div>
+                               <div className={`col-sm-8 ${styles.value}`}><strong>{formatAmount(block.TotalAmount)}</strong> uks</div>
+                             </div>
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'The validator who successfuly include the block onto blockshain.'} />
+                                 Validator</div>
+                               <div className={`col-sm-8 ${styles.value}`}>
+                                 <Link href={`/address/${block.Validator}`}>
+                                   <a className={styles.valueWithLink}>{block.Validator}</a>
+                                 </Link>
+                                 &nbsp;&nbsp; in {block.BuildTime} ms
+                               </div>
+                             </div>
+       
+       
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'For each block, the validator is rewarded with a finite amount of Ukusi on top of the fees paid for all transactions in the block.'} />
+                                 Block Reward</div>
+                               <div className={`col-sm-8 ${styles.value}`}><strong>{formatFee(block.TotalReward)}</strong> uks</div>
+                             </div>
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'The hash of all the hashes of all the transactions that are part of a block.'} />
+                                 Merkle Root </div>
+                               <div className={`col-sm-8 ${styles.value}`}>{block.MerkleRoot}</div>
+                             </div>
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'Total bytes of the block.'} />
+                                 Size</div>
+                               <div className={`col-sm-8 ${styles.value}`}>{formatBytes(block.Size)} bytes</div>
+                             </div>
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'The hash of the block header of this block.'} />
+                                 Hash </div>
+                               <div className={`col-sm-8 ${styles.value}`}>{block.Hash}</div>
+                             </div>
+       
+                             <div className={`row ${styles.rowDiv}`}>
+                               <div className="col-sm-4">
+                                 <HelpTips tips={'The hash of the previous block in the blockchain.'} /> Prev. hash </div>
+                               <div className={`col-sm-8`}>
+                                 <Link href={`/blocks/hash/${block.PrevHash}`}>
+                                   <a className={styles.valueWithLink}>{block.PrevHash}</a>
+                                 </Link>
+                               </div>
+                             </div>
+       
+                           </>
                   }
                 </div>
               </div>
