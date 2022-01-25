6b554a0569088ba600422fc3577f0b0161738652
jhonkus <putukn@gmail.com>
2022-01-26 07:12:28 +0800

    update paging on blocks table

--- components/blocks/TableBlocks.tsx
+++ components/blocks/TableBlocks.tsx
@@ -5,18 +5,14 @@ import { getBlocks } from '../../grpc/useFetch';
 import Skeleton from 'react-loading-skeleton';
 
 
-const Pagination = ({ pageNum = 1 }) => {
+const Pagination = ({ pageNum = 1, isLast = false }) => {
   return (
     <nav aria-label="block paging">
       <ul className="pagination">
         {/* <li className="page-item"><a className="page-link" href={`/blocks?page=1`}>First</a></li> */}
-        <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum - 1}`}>Prev</a></li>
+        <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum <= 1 ? pageNum : pageNum - 1}`}>Prev</a></li>
         <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum}`}>{pageNum}</a></li>
-        <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum + 1}`}>{pageNum + 1}</a></li>
-        <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum + 2}`}>{pageNum + 2}</a></li>
-        <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum + 3}`}>{pageNum + 3}</a></li>
-        <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum + 4}`}>{pageNum + 4}</a></li>
-        <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum + 5}`}>Next</a></li>
+        <li className="page-item"><a className="page-link" href={`/blocks?page=${isLast ? pageNum : pageNum + 1}`}>Next</a></li>
         {/* <li className="page-item"><a className="page-link" href={`/blocks?page=${pageNum + 1}`}>Last</a></li> */}
       </ul>
     </nav>
@@ -52,7 +48,11 @@ const TableBlocks = ({ page = 1 }) => {
                 <p>Showing 25 blocks</p>
               </div>
               <div className="col d-flex justify-content-end">
-                <Pagination pageNum={page} />
+
+                {blocks.length < 25 ? <Pagination isLast={true} pageNum={page} /> :
+                  <Pagination isLast={false} pageNum={page} />
+                }
+
               </div>
             </div>
             <div className="table-responsive">
@@ -107,7 +107,9 @@ const TableBlocks = ({ page = 1 }) => {
             </div>
             <div className="row">
               <div className="col d-flex justify-content-end">
-                <Pagination pageNum={page} />
+                {blocks.length < 25 ? <Pagination isLast={true} pageNum={page} /> :
+                  <Pagination isLast={false} pageNum={page} />
+                }
               </div>
             </div>
 
