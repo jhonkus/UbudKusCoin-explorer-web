b7cb921fd5bbde960c0d639404279976fd329cec
jhonkus <putukn@gmail.com>
2022-01-27 19:42:51 +0800

    add home link on header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -36,12 +36,12 @@ const Header = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {/* 
+            
             <li className="nav-item">
               <Link href="/">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </Link>
-            </li> */}
+            </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Blockchain
