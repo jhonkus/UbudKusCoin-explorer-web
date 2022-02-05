4600eb81d721f9d5a4bf3701797f3c4452c533aa
jhonkus <putukn@gmail.com>
2022-02-05 16:24:54 +0800

    fixing header

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -15,11 +15,9 @@ export default function Layout(props: LayoutProps) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                <meta name="description" content="Ubudkuscoin is light, fast coin from Bali, Indonesia, developed by Putu Kusuma" />
-                <meta name="author" content="pKusuma aka Jhonkus" />
-                <title>UbudKusCoin | {pageTitle}</title>
-                {/* <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-                <link rel="shortcut icon" href="/static/favicon.ico" /> */}
+                <meta name="description" content="Ubudkuscoin is simple, light, fast coin developed in Bali, Indonesia by I Putu Kusuma Negara. UbudKuscoin using Proof of Stake Consesnsus. With UbudKusCoin you can send or transfer coin with other user. UbudKusCoin open to use to other use cases." />
+                <meta name="author" content="I Putu Kusuma Negara github account: Jhonkus." />
+                <title>UbudKusCoin.com | {pageTitle}</title>
             </Head>
             <div className={styles.container}>
                 <Header />
